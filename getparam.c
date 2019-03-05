250dea82a04328699f7df93fd86c174b12b50c7e
josephguscott <41111921+josephguscott@users.noreply.github.com>
2019-03-05 18:14:29 +0000

    Update getparam.c

--- getparam.c
+++ getparam.c
@@ -42,9 +42,31 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unistd.h>
 
 /* it is in ReadFile.cpp library */
+//#ifdef NP_Implementation
 void read_parameters(Params *prms, int argc, char **argv);
+//#endif
 
+#ifdef MPI_Implementation
+#ifdef MAP_Reduce
+	#include "LVB_READ_FILES/src/ReadFile.h"
+#else
+	int read_parameters(Params *prms, int argc, char **argv);
+#endif
+
+long get_random_maxaccept(void)
+/* return a random integer value, for use as the maxaccept parameter in
+ * the simulated annealing search */
+{
+    return randpint(MAXACCEPT_MAX - MAXACCEPT_MIN) + MAXACCEPT_MIN;
+}
+
+int get_default_seed(void)
+#endif
+
+//#ifdef NP_Implementation
 static int get_default_seed(void)
+//#endif
+
 /* return a default integer in the interval [0..MAX_SEED], obtained from the
  * system clock, or exit with an error message if the system time is
  * unavailable */
@@ -54,44 +76,97 @@ static int get_default_seed(void)
 
     tim = time(NULL);
     lvb_assert(tim != -1);
+    //#ifdef NP_Implementation
     ul_seed = (unsigned long) tim;
     ul_seed = ul_seed % (1UL + (unsigned long) MAX_SEED);
+    //#endif
+    #ifdef MPI_Implementation
+    srand(tim);
+    ul_seed = (unsigned long) rand() % (1UL + (unsigned long) MAX_SEED);
+    #endif
+
     lvb_assert(ul_seed <= MAX_SEED);
     return (int) ul_seed;
 
 } /* end get_default_seed() */
 
-
 void defaults_params(Params *const prms)
 /* set seed in *prms to unacceptable value, and other parameters to their
  * defaults_params from lvb.h */
 {
+    //#ifdef NP_Implementation
     prms->bootstraps = 0;	/* sensible default */
+    //#endif
 
+    #ifdef MPI_Implementation
+    #ifndef MAP_Reduce
+    prms->n_seeds_need_to_try = 1;
+    prms->n_checkpoint_interval = CHECKPOINT_INTERVAL;
+    #endif
+
+    /* by default dont read and save states */
+    prms->n_flag_save_read_states = DONT_SAVE_READ_STATES;
+    prms->n_flag_is_finished_process = CHECK_POINT_PROCESS_NOT_FINISHED;
+    prms->n_flag_is_possible_read_state_files = CHECK_POINT_NOT_READ_STATE_FILES;
     /* meaningful value that is not user-configurable */
+    #endif
+
     prms->verbose = LVB_FALSE;
 
     /* cooling schecdule Generic */
     prms->cooling_schedule = 0;
     /* default value that will usually be used */
     prms->seed = get_default_seed();
+    //#ifdef NP_Implementation
     /* original branch-swapping algorithm */
     prms->algorithm_selection = 0;
+    //#endif
 
     strcpy(prms->file_name_in, "infile");
     strcpy(prms->file_name_out, OUTTREEFNAM);
     prms->n_file_format = FORMAT_PHYLIP;
     prms->n_processors_available = 1;
+    //#ifdef NP_Implementation
     prms->n_number_max_trees = 0;			/* by default keepall of them */
+    //#endif
+
+    #ifdef MPI_Implementation
+    /* it is olnly used for tests */
+    prms->n_make_test = 0;	/* by default is not making any test */
+    #endif
 
 } /* end defaults_params() */
 
+//#ifdef NP_Implementation
 void getparam(Params *prms, int argc, char **argv)
+//#endif
+
+#ifdef MPI_Implementation
+void getparam(Params *prms, int argc, char **argv)
+#endif
+
 /* Get configuration parameters. This function fills *prms with
  * run-time configuration parameters */
 {
 	defaults_params(prms);
  /*   user_adjust(prms);*/
+    //#ifdef NP_Implementation
 	read_parameters(prms, argc, argv);
+    //#endif
+
+    #ifdef MPI_Implementation
+    #ifndef MAP_Reduce
+        int n_default_seed = prms->seed;
+    #endif
+        int n_error_code = read_parameters(prms, argc, argv);
+
+    #ifndef MAP_Reduce
+        /* change initial seed because the user defined one */
+        if (prms->seed != n_default_seed){
+	        srand(prms->seed);
+        }
+    #endif
+        return n_error_code;
+    #endif
 
 } /* end getparam() */
