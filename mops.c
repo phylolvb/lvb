bd463e6e5f8e00db35c952ed219bd6e84c285b22
josephguscott <41111921+josephguscott@users.noreply.github.com>
2019-03-05 22:06:04 +0000

    Update mops.c MPI

--- mops.c
+++ mops.c
@@ -111,6 +111,7 @@ void *alloc(const size_t bytes, const char *const msg)
 
 }	/* end alloc() */
 
+//#ifdef NP_Implementation
 void alloc_memory_to_getplen(Dataptr matrix, long **p_todo_arr, long **p_todo_arr_sum_changes, int **p_runs){
 	*p_todo_arr = (long *) alloc((matrix->nbranches - matrix->n) * sizeof(long), "alloc to count runs");
     *p_todo_arr_sum_changes = (long *) alloc(matrix->n_threads_getplen * (1 + matrix->nbranches - matrix->n) * sizeof(long), "alloc to count runs");
@@ -122,3 +123,18 @@ void free_memory_to_getplen(long **p_todo_arr, long **p_todo_arr_sum_changes, in
 	free(*p_todo_arr_sum_changes);
 	free(*p_runs);
 }
+//#endif
+
+#ifdef MPI_Implementation
+void alloc_memory_to_getplen(Dataptr matrix, long **p_todo_arr, long **p_todo_arr_sum_changes, int **p_runs) {
+	*p_todo_arr 		= (long *) alloc((matrix->nbranches - matrix->n) * sizeof(long), "alloc to count runs");
+    *p_todo_arr_sum_changes = (long *) alloc(matrix->n_threads_getplen * (1 + matrix->nbranches - matrix->n) * sizeof(long), "alloc to count runs");
+	*p_runs 		= (int *) alloc(matrix->n_threads_getplen * (matrix->nbranches - matrix->n) * sizeof(int), "alloc to count runs");
+}
+
+void free_memory_to_getplen(long **p_todo_arr, long **p_todo_arr_sum_changes, int **p_runs) {
+	free(*p_todo_arr);
+	free(*p_todo_arr_sum_changes);
+	free(*p_runs);
+}
+#endif
