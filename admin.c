c5c64e2678e1b91627c57cec3109216d3deda1e8
Joseph Guscott <41111921+josephguscott@users.noreply.github.com>
2019-04-11 01:47:52 +0100

    Code refinement

--- admin.c
+++ admin.c
@@ -40,8 +40,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "lvb.h"
 
-#ifdef NP_Implementation
-
 /**********
 
 =head1 matrix - DATA MATRIX
@@ -84,62 +82,10 @@ static void functionality_check(void)
     if (DBL_EPSILON >= LVB_EPS)
         crash("program requires greater floating point precision");
 
-    /* DBL_MANT_DIG is checked in rinit() so check not necessary here */
-
-} /* end functionality_check() */
-
-/**********
-
-=head1 lvb_initialize - INITIALIZE lvb LIBRARY
-
-=head2 SYNOPSIS
-
-void lvb_initialize(void);
-
-=head2 DESCRIPTION
-
-Initializes the LVB library. Must be called once, before any other LVB
-functions.
-
-Currently, this function just checks that some features of the system
-are suitable for use with LVB. If not, it crashes verbosely.
-
-=cut
-
-**********/
-
-void lvb_initialize(void)
-{
-    functionality_check();
-
-} /* end lvb_initialize() */
-
-#endif // #ifdef NP_Implementation // 
-
-#ifdef MPI_Implementation
-
-static void functionality_check(void)
-/* To the extent possible, check that standard functions and data types match
- * LVB's expectations. Crash verbosely if they are found not to. */
-{
-    /* time() is expected to work without error for logging the start
-     * and end time, and for generating the default random number seed */
-    if (time(NULL) == -1) crash("cannot get system time");
-
-    /* if the system is not 32-bit, 64-bit, or more, some limits will be
-     * less than documented and there may be memory allocation constraints
-     * that LVB does not allow for */
-    if ((((long) INT_MAX) < 2147483647L) || ((sizeof(void *) * CHAR_BIT) < 32) || ((sizeof(size_t) * CHAR_BIT) < 32)) {
-        crash("program requires at least a 32-bit system");
-    }
-
-    /* LVB_EPS is assumed to be bigger than DBL_EPSILON in code that guards
-     * against floating-point arithmetic problems */
-    if (DBL_EPSILON >= LVB_EPS)
-        crash("program requires greater floating point precision");
+    #ifdef MPI_Implementation
     if (!((LVB_EPS + INITIAL_INCREMENT) != INITIAL_INCREMENT))
         crash("LVB_EPS and INITIAL_INCREMENT are incompatible with floating point precision");
-
+    #endif
 
     /* DBL_MANT_DIG is checked in rinit() so check not necessary here */
 
@@ -170,6 +116,3 @@ void lvb_initialize(void)
     functionality_check();
 
 } /* end lvb_initialize() */
-
-
-#endif
