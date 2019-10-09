3f5a538c71dacd4049e48bf9370252b370b383b5
josephguscott <joseph.guscott@gmail.com>
2019-10-09 13:08:12 +0100

    Implementation merging

--- Admin.c
+++ Admin.c
@@ -8,7 +8,7 @@ and Chris Wood.
 (c) Copyright 2019 by Daniel Barker, Miguel Pinheiro, Joseph Guscott,
 Fernando Guntoro, Maximilian Strobl and Chris Wood.
 All rights reserved.
- 
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
@@ -36,87 +36,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-/* ========== admin.c - LVB library data and administration ========== */
+/* ========== Admin.c - LVB library data and administration ========== */
 
 #include "Lvb.h"
-
-#ifdef NP_Implementation
-
-/**********
-
-=head1 matrix - DATA MATRIX
-
-=head2 SYNOPSIS
-
-Dataptr matrix;
-
-=head2 DESCRIPTION
-
-Pointer to the data matrix to use with the library during the current
-program's execution. It is not possible to change the matrix used once
-it has been read from file.
-
-To discourage direct access, C<matrix> is not declared in C<lvb.h>. It
-should be declared in functions as required.
-
-=cut
-
-**********/
-
-static void functionality_check(void)
-/* To the extent possible, check that standard functions and data types match
- * LVB's expectations. Crash verbosely if they are found not to. */
-{
-    /* time() is expected to work without error for logging the start
-     * and end time, and for generating the default random number seed */
-    if (time(NULL) == -1)
-    	crash("cannot get system time");
-
-    /* if the system is not 32-bit, 64-bit, or more, some limits will be
-     * less than documented and there may be memory allocation constraints
-     * that LVB does not allow for */
-    if ((((long) INT_MAX) < 2147483647L) || ((sizeof(void *) * CHAR_BIT) < 32) || ((sizeof(size_t) * CHAR_BIT) < 32)) {
-        crash("program requires at least a 32-bit system");
-    }
-
-    /* LVB_EPS is assumed to be bigger than DBL_EPSILON in code that guards
-     * against floating-point arithmetic problems */
-    if (DBL_EPSILON >= LVB_EPS)
-        crash("program requires greater floating point precision");
-
-    /* DBL_MANT_DIG is checked in rinit() so check not necessary here */
-
-} /* end functionality_check() */
-
-/**********
-
-=head1 lvb_initialize - INITIALIZE lvb LIBRARY
-
-=head2 SYNOPSIS
-
-void lvb_initialize(void);
-
-=head2 DESCRIPTION
-
-Initializes the LVB library. Must be called once, before any other LVB
-functions.
-
-Currently, this function just checks that some features of the system
-are suitable for use with LVB. If not, it crashes verbosely.
-
-=cut
-
-**********/
-
-void lvb_initialize(void)
-{
-    functionality_check();
-
-} /* end lvb_initialize() */
-
-#endif // #ifdef NP_Implementation // 
-
-#ifdef MPI_Implementation
 
 static void functionality_check(void)
 /* To the extent possible, check that standard functions and data types match
@@ -137,39 +59,17 @@ static void functionality_check(void)
      * against floating-point arithmetic problems */
     if (DBL_EPSILON >= LVB_EPS)
         crash("program requires greater floating point precision");
+    #ifdef MPI_Implementation
     if (!((LVB_EPS + INITIAL_INCREMENT) != INITIAL_INCREMENT))
         crash("LVB_EPS and INITIAL_INCREMENT are incompatible with floating point precision");
-
+    #endif // MPI_Implementation #121
 
     /* DBL_MANT_DIG is checked in rinit() so check not necessary here */
 
-} /* end functionality_check() */
-
-/**********
-
-=head1 lvb_initialize - INITIALIZE lvb LIBRARY
-
-=head2 SYNOPSIS
-
-void lvb_initialize(void);
-
-=head2 DESCRIPTION
-
-Initializes the LVB library. Must be called once, before any other LVB
-functions.
-
-Currently, this function just checks that some features of the system
-are suitable for use with LVB. If not, it crashes verbosely.
-
-=cut
-
-**********/
+} // end functionality_check()
 
 void lvb_initialize(void)
 {
     functionality_check();
 
-} /* end lvb_initialize() */
-
-
-#endif
+} // end lvb_initialize()
--- Cleanup.c
+++ Cleanup.c
@@ -40,31 +40,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "Lvb.h"
 
-#ifdef NP_Implementation
-
-Lvb_bool cleanup(void)
-/* prevent apparent memory leaks to help debugging, log end time; return
- * LVB_TRUE on write error to stdout, LVB_FALSE otherwise */
-{
-    time_t endtim;	/* time at end of run */
-    Lvb_bool val;	/* return value */
-
-    endtim = time(NULL);
-    // printf("\n");
-    // printf("Ending at: %s", ctime(&endtim));
-    printf("\n");
-
-    /* log file won't be used again */
-    fflush(stdout);
-    if (ferror(stdout) != 0) val = LVB_TRUE;
-    else val = LVB_FALSE;
-    return val;
-} /* end cleanup() */
-
-#endif // #ifdef NP_Implementation //
-
-#ifdef MPI_Implementation
-
 Lvb_bool cleanup(void)
 /* prevent apparent memory leaks to help debugging, log end time; return
  * LVB_TRUE on write error to stdout, LVB_FALSE otherwise */
@@ -78,13 +53,9 @@ Lvb_bool cleanup(void)
     printf("\n");
 
     /* log file won't be used again */
-#ifdef MAP_REDUCE_SINGLE
     fflush(stdout);
     if (ferror(stdout) != 0) val = LVB_TRUE;
     else val = LVB_FALSE;
-#endif
-
+    
     return val;
 } /* end cleanup() */
-
-#endif
