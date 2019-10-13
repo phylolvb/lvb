aa9cba88866fe9c0ea0ccee49d13cb180b2d344e
josephguscott <joseph.guscott@gmail.com>
2019-10-13 15:27:11 +0100

    merging_implementations

--- Err.c
+++ Err.c
@@ -40,43 +40,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "Lvb.h"
 
-#ifdef NP_Implementation
-
-/**********
-
-=head1 crash - CRASH VERBOSELY
-
-=head2 SYNOPSIS
-
-    void crash(const char *fmt, ...);
-
-=head2 DESCRIPTION
-
-Prints a message consisting of 'FATAL ERROR: ' followed by a
-user-supplied, C<vfprintf>-acceptable message. Then calls C<cleanup()>
-and exits abnormally.
-
-=head2 PARAMETERS
-
-=head3 INPUT
-
-=over 4
-
-=item fmt
-
-Pointer to the first character in a format string acceptable to
-C<vfprintf()>.
-
-=item ...
-
-Any parameters whose presence is dictated by C<fmt>.
-
-=back
-
-=cut
-
-**********/
-
 void crash(const char *const fmt, ...)
 {
 	const char *const warning = "\nFATAL ERROR";	/* dire warning */
@@ -88,126 +51,14 @@ void crash(const char *const fmt, ...)
 	va_end(args);
 	printf("\n");
 
-	cleanup();
-	exit(EXIT_FAILURE);
-
-}	/* end crash() */
-
-void lvb_assertion_fail(const char *test, const char *file, int line)
-/* Log dire warning followed by message of form "assertion failed at
- * '<file>' line <line>: <test>", and exit abnormally. This function
- * should only be called through the lvb_assert() macro. */
-{
-    crash("assertion failed at '%s' line %d: %s", file, line, test);
-}
-
-/**********
-
-=head1 scream - log dire warning
-
-=head2 SYNOPSIS
-
-    void scream(const char *format, ...);
-
-=head2 DESCRIPTION
-
-Prints a message consisting of 'ERROR: ' followed by a user-supplied,
-C<vfprintf>-acceptable message.
-
-=head2 PARAMETERS
-
-=head3 INPUT
-
-=over 4
-
-=item format
-
-Pointer to the first character in a format string acceptable to
-C<vfprintf()>.
-
-=item ...
-
-Any parameters whose presence is dictated by C<format>.
-
-=back
-
-=cut
-
-**********/
-
-void scream(const char *const format, ...)
-/* log a dire warning, partly composed of vprintf-acceptable user-supplied
- * message */
-{
-	const char *const warning = "ERROR";
-	va_list args;		/* supplied message */
-
-	va_start(args, format);
-	printf("%s: ", warning);
-	vprintf(format, args);
-	va_end(args);
-	printf("\n");
-
-	/* flush standard output so the warning is immediately visible */
-	if (fflush(stdout) == EOF)
-		crash("write error on log");	/* may not work! */
-
-}	/* end scream() */
-
-#endif // #ifdef NP_Implementation //
-
-#ifdef MPI_Implementation
-
-/**********
-
-=head1 crash - CRASH VERBOSELY
-
-=head2 SYNOPSIS
-
-    void crash(const char *fmt, ...);
-
-=head2 DESCRIPTION
-
-Prints a message consisting of 'FATAL ERROR: ' followed by a
-user-supplied, C<vfprintf>-acceptable message. Then calls C<cleanup()>
-and exits abnormally.
-
-=head2 PARAMETERS
-
-=head3 INPUT
-
-=over 4
-
-=item fmt
-
-Pointer to the first character in a format string acceptable to
-C<vfprintf()>.
-
-=item ...
-
-Any parameters whose presence is dictated by C<fmt>.
-
-=back
-
-=cut
-
-**********/
-
-void crash(const char *const fmt, ...)
-{
-	const char *const warning = "\nFATAL ERROR";	/* dire warning */
-	va_list args;					/* arguments */
-
-	va_start(args, fmt);
-	printf("%s: ", warning);
-	vprintf(fmt, args);
-	va_end(args);
-	printf("\n");
-
-	//cleanup();
+	#ifndef NP_Implementation
 	int n_error_code = 1;
 	MPI_Abort(MPI_COMM_WORLD, n_error_code);
 	exit(1);
+	#else
+	cleanup();
+	exit(EXIT_FAILURE);
+	#endif
 }	/* end crash() */
 
 void lvb_assertion_fail(const char *test, const char *file, int line)
@@ -217,40 +68,6 @@ void lvb_assertion_fail(const char *test, const char *file, int line)
 {
     crash("assertion failed at '%s' line %d: %s", file, line, test);
 }
-
-/**********
-
-=head1 scream - log dire warning
-
-=head2 SYNOPSIS
-
-    void scream(const char *format, ...);
-
-=head2 DESCRIPTION
-
-Prints a message consisting of 'ERROR: ' followed by a user-supplied,
-C<vfprintf>-acceptable message.
-
-=head2 PARAMETERS
-
-=head3 INPUT
-
-=over 4
-
-=item format
-
-Pointer to the first character in a format string acceptable to
-C<vfprintf()>.
-
-=item ...
-
-Any parameters whose presence is dictated by C<format>.
-
-=back
-
-=cut
-
-**********/
 
 void scream(const char *const format, ...)
 /* log a dire warning, partly composed of vprintf-acceptable user-supplied
@@ -270,5 +87,3 @@ void scream(const char *const format, ...)
 		crash("write error on log");	/* may not work! */
 
 }	/* end scream() */
-
-#endif
